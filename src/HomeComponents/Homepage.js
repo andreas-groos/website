b7bb4198f555ef7172c8ddfb51d90f132c655c3a
andreas-groos <andreas.groos1@gmail.com>
2018-06-30 14:37:28 -0700

    added prop validation, <HomePage /> is back to original

--- src/HomeComponents/Homepage.js
+++ src/HomeComponents/Homepage.js
@@ -7,19 +7,10 @@ import { HomeNews } from './HomeNews'
 import { HomeEvents } from './HomeEvents'
 import { SocialIcons } from './SocialIcons'
 import { MailingList } from './MailingList'
-import Spinner from './Spinner'
-
-
-const height = {
-  height: '200px'
-}
 
 export const Homepage = () => (
   <main>
     <h1 style={{ color: '#568f56' }}>Home Components</h1>
-    <div style={height}>
-      <Spinner loading={true} />
-    </div>
     <HomeSplash />
     <HomeDonations />
     <WhatIs />
